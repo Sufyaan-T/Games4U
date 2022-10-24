aeff029c8ee28285394753e7df7b9341dec89193
Zain Khel <210126981@aston.ac.uk>
2022-10-24 19:26:04 +0100

    Log In Form
    - Finishing up the final form
    - adding in error messages
    - Focusing on Java Script

--- logInForm/logIn.js
+++ logInForm/logIn.js
@@ -1,17 +1,59 @@
-doccument.addEventListener("DOMContentLoader", () => {
-    const loginForm = documet.querySelector("#login");
-    const createAccontForm = document.querySelector("createAccount");
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".formMessage");
 
-    doccument.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    messageElement.textContent = message;
+    messageElement.classList.remove("formMessageSuccess, formMessageError");
+    messageElement.classList.add(`formMessage  ${type}`)
+};
+
+function setInputError(inputElement, message){
+    inputElement.classList.add("formInputError");
+    inputElement.parentElement.querySelector(".formInputErrorMessage").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("formInputError");
+    inputElement.parentElement.querySelector(".formInputErrorMessage").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logInForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.add("formHidden");
-        createAccontForm.classList.remove("formHidden")
+        logInForm.classList.add("formHidden");
+        createAccountForm.classList.remove("formHidden");
     });
 
-    doccument.querySelector("#linkLogIn").addEventListener("click", e => {
+    document.querySelector("#linkLogIn").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.remove("formHidden");
-        createAccontForm.classList.add("formHidden")
+        logInForm.classList.remove("formHidden");
+        createAccountForm.classList.add("formHidden");
+    });
+
+    logInForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // perform ajax/fetch login
+        
+        setFormMessage(logInForm, "error", "UserName/Password Combination is Invalid");
+    });
+
+    document.querySelectorAll(".formInput").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id == "signupPassword" && e.target.value.length > 0 && e.target.value.length <8) {
+                setInputError(inputElement, "Password Must Be More Than 8 Characters")
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
     });
 
 });
+
+
+
+
